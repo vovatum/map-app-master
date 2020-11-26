9c13e56228038c92aff410619ebd6e096034d5c2
Tim Seryakov <t.seryakov@gmail.com>
2020-11-26 13:17:53 +0300

    improved placemark data render
    Took 13 minutes

--- src/components/MapPage.tsx
+++ src/components/MapPage.tsx
@@ -14,10 +14,23 @@ export const MapPage = React.memo((props: PropsType) => {
         <div>
             <YMaps>
                 <div className="mapWrapper">
-                    <Map state={{center: cityCoordinates, zoom: 12}} width={'100%'} height={'100vh'}>
-                        {schools.map((school) => <Placemark geometry={school.geometry.coordinates} properties={{
-                            iconCaption: school.properties.name,
-                        }}/>)}
+                    <Map state={{center: cityCoordinates, zoom: 12}} width={'100%'} height={'100vh'}
+                         modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+                    >
+
+                        {schools.map((school) =>
+                          <Placemark geometry={school.geometry.coordinates}
+                                     properties={{ iconCaption: school.properties.name,
+                                                   balloonContentHeader: school.properties.name,
+                                                   balloonContentBody: school.properties.description,
+                                                   hintContent: school.properties.name
+                                                 }}
+                                     options={{
+                                       preset: "islands#blueLeisureIcon",
+                                       hideIconOnBalloonOpen: false
+                                     }}
+
+                          />)}
                     </Map>
                 </div>
             </YMaps>
