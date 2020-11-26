6b62bec3d90e582c80eb2cc1c95cf9f507273edf
Tim Seryakov <t.seryakov@gmail.com>
2020-11-26 15:26:09 +0300

    another try
    Took 8 minutes

--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import {store} from './state/store';
 import {BrowserRouter} from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
-import swConfig from "./swConfig";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
@@ -23,4 +22,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.register(swConfig);
+serviceWorkerRegistration.register();
